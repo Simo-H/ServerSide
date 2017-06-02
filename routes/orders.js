c406d444f638949367dc4a2573d2e62c3d9404e9
Simo <a>
2017-06-02 22:00:19 +0300

    simo

--- routes/orders.js
+++ routes/orders.js
@@ -3,19 +3,18 @@
  */
 var express = require('express');
 var router = express.Router();
-var serverUtils = require('../Server.js');
 /* GET users listing. */
 
 var serverUtils = require('../Server.js');
 var dateFormat = require('dateformat');
 /* GET home page. */
 
-router.post('/login', function (req, res) {
-    var query= "SELECT first_name, last_name FROM Clients WHERE email_address = '"+ req.body['email_address'] +"' AND password = '"+req.body['password']+"'";
+router.get('/previousOrders', function (req, res) {
+    var query= "SELECT * FROM Orders WHERE client_id="+req.query.client_id;
     serverUtils.Select(query).then(function (value) {res.send(value);}).catch(function (error) {  console.log(err)})
 });
-router.post('/restorePassword', function (req, res) {
-    var query= "SELECT password FROM Clients WHERE email_address = '"+ req.body['email_address'] +"' AND security_answer = '"+req.body['security_answer']+"'";
+router.get('/orderDetails', function (req, res) {
+    var query= "SELECT * FROM Order_Line WHERE order_id="+req.query.order_id;
     serverUtils.Select(query).then(function (value) {res.send(value);}).catch(function (error) { res.send(error); console.log(err)})
 });
 
